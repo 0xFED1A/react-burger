a25f12c0e30d9c7dc416d10afefd8b3af9c167f8
Fedor Shendelov <shendelov@bigmir.net>
2022-12-05 23:15:19 +0800

    feat: add markup for BurgerIngredients component

--- src/components/BurgerIngredients/bureger-ingredients.jsx
+++ src/components/BurgerIngredients/bureger-ingredients.jsx
@@ -1,5 +1,19 @@
+import React from "react"
+import styles from "./bureger-ingredients.module.css";
+import {
+  Tab
+} from "@ya.praktikum/react-developer-burger-ui-components";
+
 export default function BurgerIngredients() {
+  // spacing from praktikum UI kit
   return (
-    null
+      <section className={styles["burger-ingredients"]}>
+        <h1>Соберите бургер</h1>
+        <div className={styles["tab-box"]}>
+          <Tab>Булки</Tab>
+          <Tab>Соусы</Tab>
+          <Tab>Начинки</Tab>
+        </div>
+      </section>
   )
 }
