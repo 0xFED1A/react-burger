829b4b5c27e02c5b81534b39cdce891d85323383
Fedor Shendelov <shendelov@bigmir.net>
2022-12-05 16:00:34 +0800

    feat: add profile-link-block markup

--- src/components/AppHeader/app-header.jsx
+++ src/components/AppHeader/app-header.jsx
@@ -1,6 +1,11 @@
 import React from "react";
-import { Logo } from "@ya.praktikum/react-developer-burger-ui-components";
-import { BurgerIcon } from "@ya.praktikum/react-developer-burger-ui-components";
+import {
+  Logo,
+  BurgerIcon,
+  ListIcon,
+  ProfileIcon
+}
+from "@ya.praktikum/react-developer-burger-ui-components";
 
 import headerStyles from "./app-header.module.css";
 
@@ -15,14 +20,18 @@ export default function AppHeader() {
           </a>
         </div>
         <div className="nav__nav-item nav-item">
+          <ListIcon className="nav-item__icon" type="primary" />
           <a className="nav-item__link" href="#" target="_self">
             Лента заказов
           </a>
         </div>
       </nav>
       <Logo className="header__logo" />
-      <div className="hearder__link-item link-item">
-
+      <div className="hearder__profile-link-block profile-link-block">
+        <ProfileIcon className="profile-link-block__icon" type="primary" />
+        <a className="profile-link-block__link" href="#" target="_self">
+          Лента заказов
+        </a>
       </div>
     </header>
   )
