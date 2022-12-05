b0748c8feed399c223e5c14d8e34d879b2da345b
Fedor Shendelov <shendelov@bigmir.net>
2022-12-05 15:54:11 +0800

    feat: add markup for AppHeader component

--- src/components/AppHeader/app-header.jsx
+++ src/components/AppHeader/app-header.jsx
@@ -1,5 +1,30 @@
+import React from "react";
+import { Logo } from "@ya.praktikum/react-developer-burger-ui-components";
+import { BurgerIcon } from "@ya.praktikum/react-developer-burger-ui-components";
+
+import headerStyles from "./app-header.module.css";
+
 export default function AppHeader() {
   return (
-    null
+    <header className="header">
+      <nav className="header__nav nav">
+        <div className="nav__nav-item nav-item">
+          <BurgerIcon className="nav-item__icon" type="primary" />
+          <a className="nav-item__link" href="#" target="_self">
+            Конструктор
+          </a>
+        </div>
+        <div className="nav__nav-item nav-item">
+          <BurgerIcon className="nav-item__icon" type="primary" />
+          <a className="nav-item__link" href="#" target="_self">
+            Лента заказов
+          </a>
+        </div>
+      </nav>
+      <Logo className="header__logo" />
+      <div className="hearder__link-item link-item">
+
+      </div>
+    </header>
   )
 }
