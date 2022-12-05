efb01070469205c515de749e1f0a990b27311d06
Fedor Shendelov <shendelov@bigmir.net>
2022-12-05 15:53:10 +0800

    feat: add tabs to app component

--- src/components/App/app.jsx
+++ src/components/App/app.jsx
@@ -3,11 +3,17 @@ import AppHeader from "../AppHeader/app-header";
 import BurgerIngredients from "../BurgerIngredients/bureger-ingredients";
 import BurgerConstructor from "../BurgerConstructor/burger-constructor";
 
+import appStyles from "./app.module.css";
+import { Tab } from "@ya.praktikum/react-developer-burger-ui-components";
+
 export default function App() {
   return (
     <>
       <AppHeader />
       <h1>Соберите бургер</h1>
+      <Tab>Булки</Tab>
+      <Tab>Соусы</Tab>
+      <Tab>Начинки</Tab>
       <BurgerIngredients />
       <BurgerConstructor />
     </>
