2a26f127258de9dedd0dc09a9361167d3f9d618e
Fedor Shendelov <shendelov@bigmir.net>
2022-12-05 23:16:12 +0800

    fix: fix markup for App component

--- src/components/App/app.jsx
+++ src/components/App/app.jsx
@@ -3,19 +3,16 @@ import AppHeader from "../AppHeader/app-header";
 import BurgerIngredients from "../BurgerIngredients/bureger-ingredients";
 import BurgerConstructor from "../BurgerConstructor/burger-constructor";
 
-import appStyles from "./app.module.css";
-import { Tab } from "@ya.praktikum/react-developer-burger-ui-components";
+import styles from "./app.module.css";
 
 export default function App() {
   return (
     <>
       <AppHeader />
-      <h1>Соберите бургер</h1>
-      <Tab>Булки</Tab>
-      <Tab>Соусы</Tab>
-      <Tab>Начинки</Tab>
-      <BurgerIngredients />
-      <BurgerConstructor />
+      <main className={styles.main}>
+        <BurgerIngredients />
+        <BurgerConstructor />
+      </main>
     </>
   )
 }
