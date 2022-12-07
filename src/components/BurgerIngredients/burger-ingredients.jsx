b34c339b56a863f1e671f93b39b5632f9b1de805
Fedor Shendelov <shendelov@bigmir.net>
2022-12-08 03:00:24 +0800

    feat: add custom scrollbar styling for both Firefox and Chrome browsers

--- src/components/BurgerIngredients/burger-ingredients.jsx
+++ src/components/BurgerIngredients/burger-ingredients.jsx
@@ -18,28 +18,125 @@ export default function BurgerIngredients() {
           <Tab>Соусы</Tab>
           <Tab>Начинки</Tab>
         </div>
-        <section className="mt-10">
-          <h3 className="text text_type_main-medium">Булки</h3> 
-          <ul className={`${styles.list} mt-6 pl-4 pr-4`}>
-            <li>
-              <article className={styles["list-item"]}>
-                <img 
-                  className="pl-4 pr-4 pb-2"
-                  src="https://via.placeholder.com/240x120" 
-                  alt="" 
-                />
-                <Counter count={1} size={"default"} extraClass="" />
-                <div className={`${styles["list-item__price-box"]} mb-2`}>
-                  <span className="text text_type_digits-default">20</span>
-                  <CurrencyIcon type="primary" />
-                </div>
-                <h5 className={`${styles["list-item__item-name"]} text text_type_main-default`}>
-                  Краторная булка N-200i
-                </h5>
-              </article> 
-            </li>
-          </ul>
-        </section>
+        <div className={styles["list-wrapper"]}>
+          <section className="mt-10">
+            <h3 className="text text_type_main-medium">Булки</h3> 
+            <ul className={`${styles.list} mt-6 pl-4 pr-4`}>
+              <li>
+                <article className={styles["list-item"]}>
+                  <img 
+                    className="pl-4 pr-4 pb-2"
+                    src="https://via.placeholder.com/240x120" 
+                    alt="" 
+                  />
+                  <Counter count={1} size={"default"} extraClass="" />
+                  <div className={`${styles["list-item__price-box"]} mb-2`}>
+                    <span className="text text_type_digits-default">20</span>
+                    <CurrencyIcon type="primary" />
+                  </div>
+                  <h5 className={`${styles["list-item__item-name"]} text text_type_main-default`}>
+                    Краторная булка N-200i
+                  </h5>
+                </article> 
+              </li>
+              <li>
+                <article className={styles["list-item"]}>
+                  <img 
+                    className="pl-4 pr-4 pb-2"
+                    src="https://via.placeholder.com/240x120" 
+                    alt="" 
+                  />
+                  <Counter count={1} size={"default"} extraClass="" />
+                  <div className={`${styles["list-item__price-box"]} mb-2`}>
+                    <span className="text text_type_digits-default">20</span>
+                    <CurrencyIcon type="primary" />
+                  </div>
+                  <h5 className={`${styles["list-item__item-name"]} text text_type_main-default`}>
+                    Краторная булка N-200i
+                  </h5>
+                </article> 
+              </li>
+            </ul>
+          </section>
+          <section className="mt-10">
+            <h3 className="text text_type_main-medium">Булки</h3> 
+            <ul className={`${styles.list} mt-6 pl-4 pr-4`}>
+              <li>
+                <article className={styles["list-item"]}>
+                  <img 
+                    className="pl-4 pr-4 pb-2"
+                    src="https://via.placeholder.com/240x120" 
+                    alt="" 
+                  />
+                  <Counter count={1} size={"default"} extraClass="" />
+                  <div className={`${styles["list-item__price-box"]} mb-2`}>
+                    <span className="text text_type_digits-default">20</span>
+                    <CurrencyIcon type="primary" />
+                  </div>
+                  <h5 className={`${styles["list-item__item-name"]} text text_type_main-default`}>
+                    Краторная булка N-200i
+                  </h5>
+                </article> 
+              </li>
+              <li>
+                <article className={styles["list-item"]}>
+                  <img 
+                    className="pl-4 pr-4 pb-2"
+                    src="https://via.placeholder.com/240x120" 
+                    alt="" 
+                  />
+                  <Counter count={1} size={"default"} extraClass="" />
+                  <div className={`${styles["list-item__price-box"]} mb-2`}>
+                    <span className="text text_type_digits-default">20</span>
+                    <CurrencyIcon type="primary" />
+                  </div>
+                  <h5 className={`${styles["list-item__item-name"]} text text_type_main-default`}>
+                    Краторная булка N-200i
+                  </h5>
+                </article> 
+              </li>
+            </ul>
+          </section>
+          <section className="mt-10">
+            <h3 className="text text_type_main-medium">Булки</h3> 
+            <ul className={`${styles.list} mt-6 pl-4 pr-4`}>
+              <li>
+                <article className={styles["list-item"]}>
+                  <img 
+                    className="pl-4 pr-4 pb-2"
+                    src="https://via.placeholder.com/240x120" 
+                    alt="" 
+                  />
+                  <Counter count={1} size={"default"} extraClass="" />
+                  <div className={`${styles["list-item__price-box"]} mb-2`}>
+                    <span className="text text_type_digits-default">20</span>
+                    <CurrencyIcon type="primary" />
+                  </div>
+                  <h5 className={`${styles["list-item__item-name"]} text text_type_main-default`}>
+                    Краторная булка N-200i
+                  </h5>
+                </article> 
+              </li>
+              <li>
+                <article className={styles["list-item"]}>
+                  <img 
+                    className="pl-4 pr-4 pb-2"
+                    src="https://via.placeholder.com/240x120" 
+                    alt="" 
+                  />
+                  <Counter count={1} size={"default"} extraClass="" />
+                  <div className={`${styles["list-item__price-box"]} mb-2`}>
+                    <span className="text text_type_digits-default">20</span>
+                    <CurrencyIcon type="primary" />
+                  </div>
+                  <h5 className={`${styles["list-item__item-name"]} text text_type_main-default`}>
+                    Краторная булка N-200i
+                  </h5>
+                </article> 
+              </li>
+            </ul>
+          </section>
+        </div>
       </section>
   )
 }
