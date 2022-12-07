88afcffd8ae6d587253d061078eb1115d09bc82f
Fedor Shendelov <shendelov@bigmir.net>
2022-12-08 04:08:38 +0800

    feat: add initial markup for BurgerConstructor component

--- src/components/BurgerConstructor/burger-constructor.jsx
+++ src/components/BurgerConstructor/burger-constructor.jsx
@@ -1,5 +1,85 @@
+import React from "react";
+import {
+  Button,
+  ConstructorElement,
+  DragIcon,
+  CurrencyIcon
+} from "@ya.praktikum/react-developer-burger-ui-components";
+
+import styles from "./burger-constructor.module.css";
+
 export default function BurgerConstructor() {
   return (
-    null
+    <section
+      className={`${styles["burger-constructor"]} mt-25`}
+      aria-label="Конструктор бургеров"
+    >
+      <div>
+        <ConstructorElement
+          text="Краторная булка N-200i (верх)"
+          thumbnail="https://via.placeholder.com/80x40"
+          price={200}
+          isLocked={false}
+          type="top"
+          extraClass=""
+          handleClose={() => console.log("Handler Ok!")}
+        />
+        <ConstructorElement
+          text="Краторная булка N-200i (верх)"
+          thumbnail="https://via.placeholder.com/80x40"
+          price={200}
+          isLocked={false}
+          extraClass=""
+          handleClose={() => console.log("Handler Ok!")}
+        />
+        <ConstructorElement
+          text="Краторная булка N-200i (верх)"
+          thumbnail="https://via.placeholder.com/80x40"
+          price={200}
+          isLocked={false}
+          extraClass=""
+          handleClose={() => console.log("Handler Ok!")}
+        />
+        <ConstructorElement
+          text="Краторная булка N-200i (верх)"
+          thumbnail="https://via.placeholder.com/80x40"
+          price={200}
+          isLocked={false}
+          extraClass=""
+          handleClose={() => console.log("Handler Ok!")}
+        />
+        <ConstructorElement
+          text="Краторная булка N-200i (верх)"
+          thumbnail="https://via.placeholder.com/80x40"
+          price={200}
+          isLocked={false}
+          extraClass=""
+          handleClose={() => console.log("Handler Ok!")}
+        />
+        <ConstructorElement
+          text="Краторная булка N-200i (верх)"
+          thumbnail="https://via.placeholder.com/80x40"
+          price={200}
+          isLocked={false}
+          extraClass=""
+          handleClose={() => console.log("Handler Ok!")}
+        />
+        <ConstructorElement
+          text="Краторная булка N-200i (верх)"
+          thumbnail="https://via.placeholder.com/80x40"
+          price={200}
+          isLocked={false}
+          type="bottom"
+          extraClass=""
+          handleClose={() => console.log("Handler Ok!")}
+        />
+      </div>
+      <DragIcon type="primary" />
+      <span>620</span>
+      <CurrencyIcon type="primary" />
+      <Button htmlType="button" type="primary" size="medium">
+        Оформить заказ
+      </Button>
+    </section>
   )
 }
