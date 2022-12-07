58d194c7e673a4cb14a8b5cc3115b1d879e90547
Fedor Shendelov <shendelov@bigmir.net>
2022-12-07 23:22:37 +0800

    feat: chagned classname constants to template literals

--- src/components/AppHeader/app-header.jsx
+++ src/components/AppHeader/app-header.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import styles from "./app-header.module.css";
+// import components from Practikum UI Kit
 import {
   Logo,
   BurgerIcon,
@@ -8,33 +8,31 @@ import {
 }
 from "@ya.praktikum/react-developer-burger-ui-components";
 
-export default function AppHeader() {
-  // spacing from praktikum UI kit
-  const headerSpacings = "pt-4 pb-4 ";
-  const navItemSpacings = "pt-4 pb-4 pr-5 pl-5 ";
-  const linkSpacings = "ml-2 ";
-  const linkTypography = "text text_type_main-default "
+// import custom styles as css-modules and UI kit styles
+import styles from "./app-header.module.css";
+import "@ya.praktikum/react-developer-burger-ui-components/dist/ui/common.css?module"
+import "@ya.praktikum/react-developer-burger-ui-components/dist/ui/box.css?module";
 
+// AppHeader component
+export default function AppHeader() {
   return (
-    <header className={headerSpacings + styles.header}>
-      <nav className={styles.nav + " " + styles["header__nav"]}>
-        <div className={navItemSpacings + styles["nav__item"]}>
+    <header className={`${styles.header} pt-4 pb-4`}>
+      <nav className={`${styles.nav} ${styles["header__nav"]}`}>
+        <div className={`pt-4 pb-4 pr-5 pl-5 ${styles["nav__item"]}`}>
           <BurgerIcon type="primary" />
           <a
-            className={linkTypography + linkSpacings + styles["nav__item-link"]}
+            className={`text text_type_main-default ml-2 ${styles["nav__item-link"]}`}
             href="#"
             target="_self"
           >
             Конструктор
           </a>
         </div>
-        <div className={navItemSpacings + styles["nav__item"]}>
+        <div className={`pt-4 pb-4 pr-5 pl-5 ${styles["nav__item"]}`}>
           <ListIcon type="secondary" />
           <a
-            className={
-              linkTypography + linkSpacings +
-              styles["nav__item-link"] + " " + styles["nav__item-link_inactive"]
-            }
+            className= {`text text_type_main-default ml-2
+              ${styles["nav__item-link"]} ${styles["nav__item-link_inactive"]}`}
             href="#"
             target="_self"
           >
@@ -43,18 +41,13 @@ export default function AppHeader() {
         </div>
       </nav>
       <Logo className="header__logo" />
-      <div className={
-          navItemSpacings +
-          styles["profile-link"] + " " +
-          styles["header__profile-link"]
-        }
+      <div className={`pt-4 pb-4 pr-5 pl-5 ${styles["profile-link"]}
+        ${styles["header__profile-link"]}`}
       >
         <ProfileIcon type="secondary" />
         <a
-          className={
-            linkTypography + linkSpacings + styles["profile-link__link"] +
-            " " + styles["profile-link__link_inactive"]
-          }
+          className={`text text_type_main-default ml-2
+            ${styles["profile-link__link"]} ${styles["profile-link__link_inactive"]}`}
           href="#"
           target="_self"
         >
