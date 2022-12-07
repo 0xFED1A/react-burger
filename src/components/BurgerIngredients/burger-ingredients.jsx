4c863319411d24b5d194c8225904116814476671
Fedor Shendelov <shendelov@bigmir.net>
2022-12-07 23:44:18 +0800

    fix: fix typos in naming - burEger to burger

--- src/components/BurgerIngredients/burger-ingredients.jsx
+++ src/components/BurgerIngredients/burger-ingredients.jsx
@@ -1,17 +1,22 @@
-import React from "react"
-import styles from "./bureger-ingredients.module.css";
+import React from "react";
 import {
-  Tab
+  Tab,
+  Counter,
+  CurrencyIcon
 } from "@ya.praktikum/react-developer-burger-ui-components";
+
+import styles from "./burger-ingredients.module.css";
+import "@ya.praktikum/react-developer-burger-ui-components/dist/ui/box.css?module";
 
 export default function BurgerIngredients() {
   // spacing and typography from praktikum UI kit
-  const headingTypography = "text text_type_main-large ";
-  const headingSpacings = "mt-10 ";
-  const tabBoxSpacings = "mt-5 ";
+  const headingTypography = " text text_type_main-large ";
+  const headingSpacings = " mt-10 ";
+  const tabBoxSpacings = " mt-5 ";
+
   return (
       <section className={styles["burger-ingredients"]}>
-        <h1 className={headingTypography + headingSpacings + styles.heading}>
+        <h1 className={styles.heading + headingSpacings}>
           Соберите бургер
         </h1>
         <div className={tabBoxSpacings + styles["tab-box"]}>
