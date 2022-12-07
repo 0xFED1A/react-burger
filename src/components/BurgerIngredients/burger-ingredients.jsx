9793a8ea744ab347903e2152ec71b2410072182a
Fedor Shendelov <shendelov@bigmir.net>
2022-12-08 02:14:25 +0800

    fix: fix missing pr-4 classname and column-gap property

--- src/components/BurgerIngredients/burger-ingredients.jsx
+++ src/components/BurgerIngredients/burger-ingredients.jsx
@@ -20,58 +20,7 @@ export default function BurgerIngredients() {
         </div>
         <section className="mt-10">
           <h3 className="text text_type_main-medium">Булки</h3> 
-          <ul className={`${styles.list} mt-6 p-4`}>
-            <li>
-              <article className={styles["list-item"]}>
-                <img 
-                  className="pl-4 pr-4 pb-2"
-                  src="https://via.placeholder.com/240x120" 
-                  alt="" 
-                />
-                <Counter count={1} size={"default"} extraClass="" />
-                <div className={`${styles["list-item__price-box"]} mb-2`}>
-                  <span className="text text_type_digits-default">20</span>
-                  <CurrencyIcon type="primary" />
-                </div>
-                <h5 className={`${styles["list-item__item-name"]} text text_type_main-default`}>
-                  Краторная булка N-200i
-                </h5>
-              </article> 
-            </li>
-            <li>
-              <article className={styles["list-item"]}>
-                <img 
-                  className="pl-4 pr-4 pb-2"
-                  src="https://via.placeholder.com/240x120" 
-                  alt="" 
-                />
-                <Counter count={1} size={"default"} extraClass="" />
-                <div className={`${styles["list-item__price-box"]} mb-2`}>
-                  <span className="text text_type_digits-default">20</span>
-                  <CurrencyIcon type="primary" />
-                </div>
-                <h5 className={`${styles["list-item__item-name"]} text text_type_main-default`}>
-                  Краторная булка N-200i
-                </h5>
-              </article> 
-            </li>
-            <li>
-              <article className={styles["list-item"]}>
-                <img 
-                  className="pl-4 pr-4 pb-2"
-                  src="https://via.placeholder.com/240x120" 
-                  alt="" 
-                />
-                <Counter count={1} size={"default"} extraClass="" />
-                <div className={`${styles["list-item__price-box"]} mb-2`}>
-                  <span className="text text_type_digits-default">20</span>
-                  <CurrencyIcon type="primary" />
-                </div>
-                <h5 className={`${styles["list-item__item-name"]} text text_type_main-default`}>
-                  Краторная булка N-200i
-                </h5>
-              </article> 
-            </li>
+          <ul className={`${styles.list} mt-6 pl-4 pr-4`}>
             <li>
               <article className={styles["list-item"]}>
                 <img 
