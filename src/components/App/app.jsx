e990ce0df17ebcc52855661b0eb1ea965dd50d32
Fedor Shendelov <shendelov@bigmir.net>
2022-12-09 05:15:22 +0800

    feat: add basic API call, remove hardcoded data

--- src/components/App/app.jsx
+++ src/components/App/app.jsx
@@ -1,19 +1,34 @@
 import React from "react"
+import { useState, useEffect } from "react";
 import AppHeader from "../AppHeader/app-header";
 import BurgerIngredients from "../BurgerIngredients/burger-ingredients";
 import BurgerConstructor from "../BurgerConstructor/burger-constructor";
-import hardCodedData from "../../utils/data";
 
 import styles from "./app.module.css";
 
 export default function App() {
-  return (
-    <>
-      <AppHeader />
-      <main className={styles.main}>
-        <BurgerIngredients ingredientsList={hardCodedData} />
-        <BurgerConstructor ingredientsList={hardCodedData} />
-      </main>
-    </>
-  )
+  const URL = "https://norma.nomoreparties.space/api/ingredients";
+  const [ingredientsList, setIngredeintsList] = useState(null);
+  useEffect(() => {
+    const getIngredientsList = async () => {
+      const response = await fetch(URL);
+      const data = await response.json();
+      setIngredeintsList(data.data);
+    };
+    getIngredientsList();
+  }, [])
+
+  if(!ingredientsList) {
+    return null;
+  } else {
+    return (
+      <>
+        <AppHeader />
+        <main className={styles.main}>
+          <BurgerIngredients ingredientsList={ingredientsList} />
+          <BurgerConstructor ingredientsList={ingredientsList} />
+        </main>
+      </>
+    )
+  }
 }
