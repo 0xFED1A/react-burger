14c42167bff073ea66418ea237296b6af48c7f56
Fedor Shendelov <shendelov@bigmir.net>
2022-12-08 22:38:15 +0800

    feat: add populateIngredients(ingType) to BurgerIngredients component

--- src/components/BurgerIngredients/burger-ingredients.jsx
+++ src/components/BurgerIngredients/burger-ingredients.jsx
@@ -7,7 +7,40 @@ import {
 
 import styles from "./burger-ingredients.module.css";
 
-export default function BurgerIngredients() {
+export default function BurgerIngredients(props) {
+
+  // this function generates markup from passed props
+  function populateIngredients(ingredientType) {
+    const ingredientsList = props.ingredientsList.filter(item => item.type == ingredientType);
+    return (
+      <>
+        {
+          ingredientsList.map(item => (
+            <li>
+              <article className={styles["list-item"]}>
+                <img
+                  className="pl-4 pr-4 pb-2"
+                  src={item.image}
+                  alt={item.name}
+                />
+                <Counter count={1} size={"default"} extraClass="" />
+                <div className={`${styles["list-item__price-box"]} mb-2`}>
+                  <span className="text text_type_digits-default">
+                    {item.price}
+                  </span>
+                  <CurrencyIcon type="primary" />
+                </div>
+                <h5 className={`${styles["list-item__item-name"]} text text_type_main-default`}>
+                  {item.name}
+                </h5>
+              </article>
+            </li>
+          ))
+        }
+      </>
+    )
+  }
+
   return (
       <section className={`${styles["burger-ingredients"]} mt-10`}>
         <h1 className={`text text_type_main-large`}>
@@ -20,115 +53,21 @@ export default function BurgerIngredients() {
         </div>
         <div className={styles["list-wrapper"]}>
           <section className="mt-10">
-            <h3 className="text text_type_main-medium">Булки</h3> 
+            <h3 className="text text_type_main-medium">Булки</h3>
             <ul className={`${styles.list} mt-6 pl-4 pr-4`}>
-              <li>
-                <article className={styles["list-item"]}>
-                  <img 
-                    className="pl-4 pr-4 pb-2"
-                    src="https://via.placeholder.com/240x120" 
-                    alt="" 
-                  />
-                  <Counter count={1} size={"default"} extraClass="" />
-                  <div className={`${styles["list-item__price-box"]} mb-2`}>
-                    <span className="text text_type_digits-default">20</span>
-                    <CurrencyIcon type="primary" />
-                  </div>
-                  <h5 className={`${styles["list-item__item-name"]} text text_type_main-default`}>
-                    Краторная булка N-200i
-                  </h5>
-                </article> 
-              </li>
-              <li>
-                <article className={styles["list-item"]}>
-                  <img 
-                    className="pl-4 pr-4 pb-2"
-                    src="https://via.placeholder.com/240x120" 
-                    alt="" 
-                  />
-                  <Counter count={1} size={"default"} extraClass="" />
-                  <div className={`${styles["list-item__price-box"]} mb-2`}>
-                    <span className="text text_type_digits-default">20</span>
-                    <CurrencyIcon type="primary" />
-                  </div>
-                  <h5 className={`${styles["list-item__item-name"]} text text_type_main-default`}>
-                    Краторная булка N-200i
-                  </h5>
-                </article> 
-              </li>
+              {populateIngredients("bun")}
             </ul>
           </section>
           <section className="mt-10">
-            <h3 className="text text_type_main-medium">Булки</h3> 
+            <h3 className="text text_type_main-medium">Соусы</h3>
             <ul className={`${styles.list} mt-6 pl-4 pr-4`}>
-              <li>
-                <article className={styles["list-item"]}>
-                  <img 
-                    className="pl-4 pr-4 pb-2"
-                    src="https://via.placeholder.com/240x120" 
-                    alt="" 
-                  />
-                  <Counter count={1} size={"default"} extraClass="" />
-                  <div className={`${styles["list-item__price-box"]} mb-2`}>
-                    <span className="text text_type_digits-default">20</span>
-                    <CurrencyIcon type="primary" />
-                  </div>
-                  <h5 className={`${styles["list-item__item-name"]} text text_type_main-default`}>
-                    Краторная булка N-200i
-                  </h5>
-                </article> 
-              </li>
-              <li>
-                <article className={styles["list-item"]}>
-                  <img 
-                    className="pl-4 pr-4 pb-2"
-                    src="https://via.placeholder.com/240x120" 
-                    alt="" 
-                  />
-                  <Counter count={1} size={"default"} extraClass="" />
-                  <div className={`${styles["list-item__price-box"]} mb-2`}>
-                    <span className="text text_type_digits-default">20</span>
-                    <CurrencyIcon type="primary" />
-                  </div>
-                  <h5 className={`${styles["list-item__item-name"]} text text_type_main-default`}>
-                    Краторная булка N-200i
-                  </h5>
-                </article> 
-              </li>
-              <li>
-                <article className={styles["list-item"]}>
-                  <img 
-                    className="pl-4 pr-4 pb-2"
-                    src="https://via.placeholder.com/240x120" 
-                    alt="" 
-                  />
-                  <Counter count={1} size={"default"} extraClass="" />
-                  <div className={`${styles["list-item__price-box"]} mb-2`}>
-                    <span className="text text_type_digits-default">20</span>
-                    <CurrencyIcon type="primary" />
-                  </div>
-                  <h5 className={`${styles["list-item__item-name"]} text text_type_main-default`}>
-                    Краторная булка N-200i
-                  </h5>
-                </article> 
-              </li>
-              <li>
-                <article className={styles["list-item"]}>
-                  <img 
-                    className="pl-4 pr-4 pb-2"
-                    src="https://via.placeholder.com/240x120" 
-                    alt="" 
-                  />
-                  <Counter count={1} size={"default"} extraClass="" />
-                  <div className={`${styles["list-item__price-box"]} mb-2`}>
-                    <span className="text text_type_digits-default">20</span>
-                    <CurrencyIcon type="primary" />
-                  </div>
-                  <h5 className={`${styles["list-item__item-name"]} text text_type_main-default`}>
-                    Краторная булка N-200i
-                  </h5>
-                </article> 
-              </li>
+              {populateIngredients("sauce")}
+            </ul>
+          </section>
+          <section className="mt-10">
+            <h3 className="text text_type_main-medium">Основное</h3>
+            <ul className={`${styles.list} mt-6 pl-4 pr-4`}>
+              {populateIngredients("main")}
             </ul>
           </section>
         </div>
