151b621082556845c3ab7faead785ef092389960
Fedor Shendelov <shendelov@bigmir.net>
2022-12-10 04:43:29 +0800

    feat: finish markup and styling for IngredientDetails component

--- src/components/IngredientDetails/ingerdient-details.jsx
+++ src/components/IngredientDetails/ingerdient-details.jsx
@@ -5,9 +5,19 @@ import styles from "./ingerdient-details.module.css"
 export default function IngredientDetails(props) {
   return (
     <div className={`${styles["ingredient-details"]} mt-4`}>
-      <p>034536</p>
-      <p>идентификатор заказа</p>
-      <div className={`${styles["ingredient-details__image"]}`}></div>
+      <p className={`${styles["ingredient-details__id"]} text text_type_digits-large`}>
+        034536
+      </p>
+      <p className="text text_type_main-medium mt-8">
+        идентификатор заказа
+      </p>
+      <div className={`${styles["ingredient-details__image"]} mt-15`}></div>
+      <p className="text text_type_main-default mt-15">
+        Ваш заказ начали готовить
+      </p>
+      <p className="text text_type_main-default mt-2 text_color_inactive mb-30">
+        Дождитесь готовности на орбитальной станции
+      </p>
     </div>
   )
 }
