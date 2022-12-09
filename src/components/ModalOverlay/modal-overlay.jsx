38b6a0229f3456b1e44b34bf9331a3303e72d0fa
Fedor Shendelov <shendelov@bigmir.net>
2022-12-09 21:38:47 +0800

    feat: add basic markup and styles for ModalOverlay component

--- src/components/ModalOverlay/modal-overlay.jsx
+++ src/components/ModalOverlay/modal-overlay.jsx
@@ -1,5 +1,11 @@
-export default function ModalOverlay() {
+import React from "react";
+
+import styles from "./modal-overlay.module.css"
+
+export default function ModalOverlay(props) {
   return (
-    null
+    <div className={styles["modal-overlay"]}>
+      {props.children}
+    </div>
   )
 }
