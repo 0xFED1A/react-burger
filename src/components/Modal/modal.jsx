9456c55ba2e593645f54ab6dd82bd2e7c785fcaf
Fedor Shendelov <shendelov@bigmir.net>
2022-12-09 23:29:38 +0800

    feat: add basic event listener features to Modal component

--- src/components/Modal/modal.jsx
+++ src/components/Modal/modal.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useEffect, useRef } from "react";
 import PortalReactDOM from 'react-dom';
 import {
   CloseIcon
@@ -8,6 +9,35 @@ import ModalOverlay from "../ModalOverlay/modal-overlay";
 import styles from "./modal.module.css";
 
 export default function Modal(props) {
+    function handleCloseOnEscape(event) {
+      if (event.key.toLowerCase() === "escape") {
+        console.log("esc pressed");
+      };
+    }
+
+    function handleCloseOnOverlayClick() {
+      console.log("overlay clicked");
+    };
+
+    function handleCloseOnButtonClick() {
+      console.log("button click!");
+    }
+
+  const closeButtonRef = useRef(null);
+
+  useEffect(() => {
+    document.addEventListener("keydown", handleCloseOnEscape);
+    const closeButton = closeButtonRef.current;
+    closeButton.addEventListener("click", handleCloseOnButtonClick);
+
+
+    return () => {
+      closeButton.removeEventListener("click", handleCloseOnButtonClick);
+      document.removeEventListener("keydown", handleCloseOnEscape);
+    }
+  }, []);
+
+
   const modalEntrypointElement = document.getElementById("modal");
   return PortalReactDOM.createPortal(
     (
@@ -15,7 +45,11 @@ export default function Modal(props) {
         <div className={`${styles.modal}`}>
           <div className={`${styles["modal__header"]} mt-10 mr-10 ml-10`}>
             <span className="text text_type_main-large">{props.heading}</span>
-            <button className={`${styles["modal__button-close"]}`} type="button">
+            <button
+              className={`${styles["modal__button-close"]}`}
+              type="button"
+              ref={closeButtonRef}
+            >
               <CloseIcon type="primary" />
             </button>
             {props.children}
