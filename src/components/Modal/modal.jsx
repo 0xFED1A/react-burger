f8d3ecf66439fae45ab01866769c966e42863577
Fedor Shendelov <shendelov@bigmir.net>
2022-12-09 21:40:52 +0800

    feat: add ModalOverlay component to markup in Modal component

--- src/components/Modal/modal.jsx
+++ src/components/Modal/modal.jsx
@@ -1,11 +1,24 @@
 import React from "react";
 import PortalReactDOM from 'react-dom';
+import {
+  CloseIcon
+} from "@ya.praktikum/react-developer-burger-ui-components";
+import ModalOverlay from "../ModalOverlay/modal-overlay";
 
-export default function Modal() {
+import styles from "./modal.module.css";
+
+export default function Modal(props) {
   const modalEntrypointElement = document.getElementById("modal");
   return PortalReactDOM.createPortal(
     (
-      null
+      <ModalOverlay>
+        <div className={`${styles.modal}`}>
+          <div className={`${styles["modal__header"]}`}>
+            <span>{props.heading}</span>
+            <CloseIcon type="primary" />
+          </div>
+        </div>
+      </ModalOverlay>
     ),
     modalEntrypointElement
   );
