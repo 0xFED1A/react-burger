d5ff6e2578fa497e1fb6e70f5c45a8721f608f48
Fedor Shendelov <shendelov@bigmir.net>
2022-12-11 23:10:05 +0800

    feat: add styles to IngredientCategory component

--- src/components/IngredientsCategory/ingredients-category.jsx
+++ src/components/IngredientsCategory/ingredients-category.jsx
@@ -11,28 +11,28 @@ export default function IngredientsCategory({
     <>
       <section className="mt-10">
         <h3 className="text text_type_main-medium">{categoryName}</h3>
-        <ul className={`${styles.list} mt-6 pl-4 pr-4`}>
+        <ul className={`${styles.ingredients} mt-6 pl-4 pr-4`}>
           {
             sameTypeIngredients.map(item => {
               <li
                 key={item._id}
                 onClick={() => onButtonClick(item._id)}
               >
-                <article className={styles["list-item"]}>
+                <article className={styles.ingredient}>
                   <img
                     className="pl-4 pr-4 pb-2"
                     src={item.image}
                     alt={item.name}
                   />
                   <Counter count={1} size={"default"} extraClass="" />
-                  <div className={`${styles["list-item__price-box"]} mb-2`}>
+                  <div className={`${styles["ingredient__price-box"]} mb-2`}>
                     <span className="text text_type_digits-default">
                       {item.price}
                     </span>
                     <CurrencyIcon type="primary" />
                   </div>
                   <h5 className={`
-                      ${styles["list-item__item-name"]}
+                      ${styles["ingredient__item-name"]}
                       text text_type_main-default
                     `}>
                     {item.name}
