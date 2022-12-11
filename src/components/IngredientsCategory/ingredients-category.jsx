b41b0e55c83a096574d462cb5f066dfa89058b7b
Fedor Shendelov <shendelov@bigmir.net>
2022-12-11 23:00:47 +0800

    fix: add forgotten li element to IngredientsCategory component markup

--- src/components/IngredientsCategory/ingredients-category.jsx
+++ src/components/IngredientsCategory/ingredients-category.jsx
@@ -2,35 +2,47 @@ import React from "react";
 import { Counter } from "@ya.praktikum/react-developer-burger-ui-components";
 import styles from "./ingredients-category.module.css";
 
-export default function IngredientsCategory({sameTypeIngredients, onButtonClick}) {
+export default function IngredientsCategory({
+  sameTypeIngredients,
+  onButtonClick,
+  categoryName
+}) {
   return (
     <>
-    {
-      sameTypeIngredients.map(item => {
-        <li
-          key={item._id}
-          onClick={() => onButtonClick(item._id)}
-        >
-          <article className={styles["list-item"]}>
-            <img
-              className="pl-4 pr-4 pb-2"
-              src={item.image}
-              alt={item.name}
-            />
-            <Counter count={1} size={"default"} extraClass="" />
-            <div className={`${styles["list-item__price-box"]} mb-2`}>
-              <span className="text text_type_digits-default">
-                {item.price}
-              </span>
-              <CurrencyIcon type="primary" />
-            </div>
-            <h5 className={`${styles["list-item__item-name"]} text text_type_main-default`}>
-              {item.name}
-            </h5>
-          </article>
-        </li>
-      })
-    }
+      <section className="mt-10">
+        <h3 className="text text_type_main-medium">{categoryName}</h3>
+        <ul className={`${styles.list} mt-6 pl-4 pr-4`}>
+          {
+            sameTypeIngredients.map(item => {
+              <li
+                key={item._id}
+                onClick={() => onButtonClick(item._id)}
+              >
+                <article className={styles["list-item"]}>
+                  <img
+                    className="pl-4 pr-4 pb-2"
+                    src={item.image}
+                    alt={item.name}
+                  />
+                  <Counter count={1} size={"default"} extraClass="" />
+                  <div className={`${styles["list-item__price-box"]} mb-2`}>
+                    <span className="text text_type_digits-default">
+                      {item.price}
+                    </span>
+                    <CurrencyIcon type="primary" />
+                  </div>
+                  <h5 className={`
+                      ${styles["list-item__item-name"]}
+                      text text_type_main-default
+                    `}>
+                    {item.name}
+                  </h5>
+                </article>
+              </li>
+            })
+          }
+        </ul>
+      </section>
     </>
   )
 }
