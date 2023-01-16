1f3f5e72c3f25f2418e241a3c01dfffbe0610e3a
Fedor Shendelov <shendelov@bigmir.net>
2023-01-16 12:30:01 +0800

    feat: create store and attach it to app
    - add required imports
    
    - add store with rootReducer and middleware
    
    - wrap store provider around app

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { compose, createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+
 import App from './components/App/app';
+import rootReducer from './services/reducers/root-reducer';
 
 import './index.css';
 import "@ya.praktikum/react-developer-burger-ui-components/dist/ui/common.css?module"
@@ -12,10 +17,14 @@ const rootComponent = ReactDOM.createRoot(rootElement as HTMLElement);
 const composeEnhancers = window['__REDUX_DEVTOOLS_EXTENSION_COMPOSE__'] as typeof compose || compose;
 const enhancer = composeEnhancers(applyMiddleware(thunk));
 
+const store = createStore(rootReducer, enhancer);
+
 rootComponent.render(
   (
     <React.StrictMode>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </React.StrictMode>
   )
 );
