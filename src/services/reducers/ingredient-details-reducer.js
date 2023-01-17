c412e1cbe34b3008b94ca0bb0941aacd9b483be5
Fedor Shendelov <shendelov@bigmir.net>
2023-01-17 10:41:42 +0800

    feat: add basic functionality in ingredient-details action/reducer

--- src/services/reducers/ingredient-details-reducer.js
+++ src/services/reducers/ingredient-details-reducer.js
@@ -1,12 +1,21 @@
+import {
+  SET_CURRENT_INGREDIENT,
+  CLEAR_CURRENT_INGREDIENT
+} from "../actions/ingredient-details-action";
+
 const initialValue = {
-  name: "",
-  calories: 0,
-  proteins: 0,
-  fat: 0,
-  carbohydrates: 0
+  id: null
 }
 
 // this reducer contains data for current previewing ingredient
 export default function ingredientDetailsReducer(state = initialValue, action) {
-  return state;
+  switch (action.type) {
+    case SET_CURRENT_INGREDIENT: {
+      return {id: action.id};
+    } case CLEAR_CURRENT_INGREDIENT: {
+      return {id: null}
+    } default: {
+      return state;
+    }
+  }
 }
