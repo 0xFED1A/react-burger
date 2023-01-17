c028498825027e099a909437b3699fa0b202dcff
Fedor Shendelov <shendelov@bigmir.net>
2023-01-17 10:41:13 +0800

    feat: add basic functionality in burger-ingredients action/reducer

---
+++ src/services/actions/burger-ingredients-action.js
@@ -0,0 +1,19 @@
+import { getIngredientsList } from "../../utils/api";
+
+export const GET_INGREDIENTS_REQUEST = "GET_ITEMS_REQUEST";
+export const GET_INGREDIENTS_SUCCESS = "GET_INGREDIENTS_SUCCESS";
+export const GET_INGREDIENTS_FAILED = "GET_ITEMS_FAILED";
+
+// this middleware performs API call to get ingredients data from server
+export function getIngredientsFromServer() {
+  return function(dispatch) {
+    dispatch({type: GET_INGREDIENTS_REQUEST})
+    getIngredientsList().then(data => {
+      if (data.constructor.name === "Error") {
+        dispatch({type: GET_INGREDIENTS_FAILED, errorMsg: data})
+      } else {
+        dispatch({type: GET_INGREDIENTS_SUCCESS, ingredients: data.data})
+      }
+    });
+  }
+}
--- src/services/reducers/burger-ingredients-reducer.js
+++ src/services/reducers/burger-ingredients-reducer.js
@@ -1,10 +1,39 @@
+import {
+  GET_INGREDIENTS_REQUEST,
+  GET_INGREDIENTS_FAILED,
+  GET_INGREDIENTS_SUCCESS,
+} from "../actions/burger-ingredients-action";
+
 const initialValue = {
-  bun: null,
+  buns: [],
   mains: [],
-  sauces: []
+  sauces: [],
+  requesting: false,
+  success: false
 }
 
-// this reducer contains list of ingredients in current burger constructor
+// this reducer generates list of available ingredients fetched from server
 export default function burgerIngredientsReducer(state = initialValue, action) {
-  return state;
+  switch(action.type) {
+    case GET_INGREDIENTS_REQUEST: {
+      return {...state, requesting: true, success: false};
+    }
+    case GET_INGREDIENTS_FAILED: {
+      //TODO: move console.log() out of reducer!
+      console.log(action.errorMsg);
+      return {...state, requesting: false, success: false};
+    }
+    case GET_INGREDIENTS_SUCCESS: {
+      return {
+        buns: action.ingredients.filter(item => item.type === "bun"),
+        mains: action.ingredients.filter(item => item.type === "main"),
+        sauces: action.ingredients.filter(item => item.type === "sauce"),
+        requesting: false,
+        success: true
+      }
+    }
+    default: {
+      return state;
+    }
+  }
 }
