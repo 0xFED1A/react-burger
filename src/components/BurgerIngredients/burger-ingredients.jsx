8db74bf88f0aff0933cae8c291f55cf7be35ac60
Fedor Shendelov <shendelov@bigmir.net>
2023-01-17 18:17:55 +0800

    feat: attach store to BurgerIngredients
    - change buns, main, sauces variables to bunsList, mainsList, saucesList
    
    - reimplement each of them CAUTION: this array.reduce() functions may cause severe brain damage, u've been warned!
    
    - alter some JSX code to work with new variables

--- src/components/BurgerIngredients/burger-ingredients.jsx
+++ src/components/BurgerIngredients/burger-ingredients.jsx
@@ -1,15 +1,19 @@
 import React from "react";
 import { useState, useMemo } from "react";
 import { useSelector } from "react-redux";
+
 import Modal from "../Modal/modal"
 import IngredientDetails from "../IngredientDetails/ingerdient-details";
 import IngredientsCategory from "../IngredientsCategory/ingredients-category";
 import { Tab } from "@ya.praktikum/react-developer-burger-ui-components";
+
 import styles from "./burger-ingredients.module.css";
 
 export default function BurgerIngredients() {
   // this states is required to control rendering
   // of Modal component
+//const [modalData, setModalData] =
+//  useState({isOpened: false, content: null, header: null});
 
   // this functions scrolls box with ingredients
   // based on passed tab value
@@ -55,18 +59,48 @@ export default function BurgerIngredients() {
 //  setModalData({isOpened: false, content: null, header: null})
 //}
 
-  const buns = useMemo(
-    () => ingredientsList.filter((item) => item.type === 'bun'),
-    [ingredientsList]
-  );
-  const mains = useMemo(
-    () => ingredientsList.filter((item) => item.type === 'main'),
-    [ingredientsList]
-  );
-  const sauces = useMemo(
-    () => ingredientsList.filter((item) => item.type === 'sauce'),
-    [ingredientsList]
-  );
+  const {buns, mains, sauces} = useSelector(store => store.ingredients);
+  const {bun, mainsAndSauces} = useSelector(store => store.usedIngredients);
+
+  const bunsList = useMemo(
+    () => {
+      const usedBun = bun;
+      return buns.reduce((acc, oneOfAvailableBuns) => {
+        acc.push({
+          data: oneOfAvailableBuns,
+          quantity: oneOfAvailableBuns._id === usedBun ? 1 : 0
+        });
+        return acc;
+      }, []);
+    }, [buns, bun]);
+
+  const mainsList = useMemo(
+    () => {
+      return mains.reduce((acc, oneOfAvailableMains) => {
+        acc.push({
+          data: oneOfAvailableMains,
+          quantity:
+            mainsAndSauces.filter(oneOfUsedMains => (
+              oneOfUsedMains === oneOfAvailableMains._id)
+            ).length
+        });
+        return acc;
+      }, []);
+    }, [mains, mainsAndSauces]);
+
+  const saucesList = useMemo(
+    () => {
+      return sauces.reduce((acc, oneOfAvailableSauces) => {
+        acc.push({
+          data: oneOfAvailableSauces,
+          quantity:
+            mainsAndSauces.filter(oneOfUsedSauces => (
+              oneOfUsedSauces === oneOfAvailableSauces._id)
+            ).length
+        });
+        return acc;
+      }, []);
+    }, [sauces, mainsAndSauces]);
 
   return (
     <>
@@ -91,23 +125,25 @@ export default function BurgerIngredients() {
             Начинки
           </Tab>
         </div>
+        {
         <div className={styles["list-wrapper"]}>
           <IngredientsCategory
             categoryName="Булки"
-            sameCategoryIngredients={buns}
-            onButtonClick={handleOpenModal}
+            sameCategoryIngredients={bunsList}
+            onButtonClick={/*handleOpenModal*/ null}
           />
           <IngredientsCategory
             categoryName="Соусы"
-            sameCategoryIngredients={sauces}
-            onButtonClick={handleOpenModal}
+            sameCategoryIngredients={saucesList}
+            onButtonClick={/*handleOpenModal*/ null}
           />
           <IngredientsCategory
             categoryName="Начинки"
-            sameCategoryIngredients={mains}
-            onButtonClick={handleOpenModal}
+            sameCategoryIngredients={mainsList}
+            onButtonClick={/*handleOpenModal*/ null}
           />
         </div>
+        }
       </section>
     </>
   )
