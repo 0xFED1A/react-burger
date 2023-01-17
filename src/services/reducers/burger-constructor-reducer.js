8ca0b1abff665a16995b1f6247949954b4d32d9b
Fedor Shendelov <shendelov@bigmir.net>
2023-01-17 10:40:17 +0800

    feat: add basic functionality in burget-constructor action/reducer

--- src/services/reducers/burger-constructor-reducer.js
+++ src/services/reducers/burger-constructor-reducer.js
@@ -1,10 +1,53 @@
+import {
+  ADD_INGREDIENT,
+  REMOVE_INGREDIENT,
+  SWAP_INGREDIENT_POSITION
+} from "../actions/burger-constructor-action";
+
 const initialValue = {
-  buns: [],
-  mains: [],
-  sauces: []
+  bun: null,
+  mainsAndSauces: []
 }
 
-// this reducer cotains list of available ingredients fetched from server
+// this reducer generates list of ingredients in current burger constructor
 export default function burgerConstructorReducer(state = initialValue, action) {
-  return state;
+  switch (action.type) {
+    case ADD_INGREDIENT: {
+      if (action.isBun) {
+        return {...state, bun: action.id};
+      } else {
+        const newMainsAndSauces = [...state.mainsAndSauces];
+        newMainsAndSauces.slice(action.position, 0, action.id);
+        return ({ ...state, mainsAndSauces: newMainsAndSauces});
+      }
+    }
+    case REMOVE_INGREDIENT: {
+      if (action.isBun) {
+        return {...state};
+      } else {
+        const newMainsAndSauces = [...state.mainsAndSauces];
+        newMainsAndSauces.slice(action.position, 1);
+        return ({ ...state , mainsAndSauces: newMainsAndSauces});
+      }
+    }
+    case SWAP_INGREDIENT_POSITION: {
+      if (action.isBun) {
+        return {...state};
+      } else {
+        const newMainsAndSauces = [...state.mainsAndSauces];
+        newMainsAndSauces.slice(action.oldPosition, 1);
+        newMainsAndSauces.slice(
+          action.newPosition, 0, state.mainsAndSauces[action.oldPosition]
+        );
+        return ({
+          ...state,
+          mainsAndSauces:
+            [...state.mainsAndSauces]
+        });
+      }
+    }
+    default: {
+      return state;
+    }
+  }
 }
