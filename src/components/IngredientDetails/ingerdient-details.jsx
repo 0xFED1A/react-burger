3521449baae65455ca7e3b4eb7b52cfc37239270
Fedor Shendelov <shendelov@bigmir.net>
2023-01-18 17:46:03 +0800

    style: fix 80col formatting in IngredientDetails

--- src/components/IngredientDetails/ingerdient-details.jsx
+++ src/components/IngredientDetails/ingerdient-details.jsx
@@ -20,20 +20,36 @@ export default function IngredientDetails() {
       <h3 className="text text_type_main-medium mt-4">{ingredient.name}</h3>
       <div className={`${styles["ingredient-details__stats"]} mt-8 mb-15`}>
         <div className={`${styles["ingredient-details__stat"]}`}>
-          <p className="text text_type_main-default text_color_inactive">Калории, ккал</p>
-          <p className="text text_type_digits-default text_color_inactive">{ingredient.calories}</p>
+          <p className="text text_type_main-default text_color_inactive">
+            Калории, ккал
+          </p>
+          <p className="text text_type_digits-default text_color_inactive">
+            {ingredient.calories}
+          </p>
         </div>
         <div className={`${styles["ingredient-details__stat"]}`}>
-          <p className="text text_type_main-default text_color_inactive">Белки, г</p>
-          <p className="text text_type_digits-default text_color_inactive">{ingredient.proteins}</p>
+          <p className="text text_type_main-default text_color_inactive">
+            Белки, г
+          </p>
+          <p className="text text_type_digits-default text_color_inactive">
+            {ingredient.proteins}
+          </p>
         </div>
         <div className={`${styles["ingredient-details__stat"]}`}>
-          <p className="text text_type_main-default text_color_inactive">Жиры, г</p>
-          <p className="text text_type_digits-default text_color_inactive">{ingredient.fat}</p>
+          <p className="text text_type_main-default text_color_inactive">
+            Жиры, г
+          </p>
+          <p className="text text_type_digits-default text_color_inactive">
+            {ingredient.fat}
+          </p>
         </div>
         <div className={`${styles["ingredient-details__stat"]}`}>
-          <p className="text text_type_main-default text_color_inactive">Углеводы, г</p>
-          <p className="text text_type_digits-default text_color_inactive">{ingredient.carbohydrates}</p>
+          <p className="text text_type_main-default text_color_inactive">
+            Углеводы, г
+          </p>
+          <p className="text text_type_digits-default text_color_inactive">
+            {ingredient.carbohydrates}
+          </p>
         </div>
       </div>
     </div>
