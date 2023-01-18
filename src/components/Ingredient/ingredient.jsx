b8365529957181c2152d20e6de6add7c820a1157
Fedor Shendelov <shendelov@bigmir.net>
2023-01-18 15:37:19 +0800

    feat: add "flat" styles and JSX markup for Ingredient component

--- src/components/Ingredient/ingredient.jsx
+++ src/components/Ingredient/ingredient.jsx
@@ -2,17 +2,29 @@ import React from "react";
 
 import {
   Counter,
-  CurrencyIcon
+  CurrencyIcon,
+  ConstructorElement,
+  DragIcon
 } from "@ya.praktikum/react-developer-burger-ui-components";
 
 import styles from "./ingredient.module.css";
 
-export default function Ingredient({name, price, image, quantity}) {
+export default function Ingredient({
+  isFlat,
+  name,
+  image,
+  price,
+  quantity,
+  type,
+  isLocked
+})
+{
   return (
+    !isFlat ?
       <article className={styles.ingredient}>
         <img className="pl-4 pr-4 pb-2" src={image} alt={name} />
         { quantity > 0 &&
-          <Counter count={quantity} size={"default"} />
+        <Counter count={quantity} size={"default"} />
         }
         <div className={`${styles["ingredient__price-box"]} mb-2`}>
           <span className="text text_type_digits-default">
@@ -20,9 +32,29 @@ export default function Ingredient({name, price, image, quantity}) {
           </span>
           <CurrencyIcon type="primary" />
         </div>
-        <h5 className={`${styles["ingredient__item-name"]} text text_type_main-default`}>
+        <h5 className=
+          {
+            `${styles["ingredient__item-name"]} text text_type_main-default`
+          }
+        >
           {name}
         </h5>
       </article>
+    :
+    <article className={`
+        ${styles["ingredient_flat"]}
+        ${type === "top" ? "ml-8 mb-4" : type === "bottom" ? "ml-8 mt-4" : ""}
+      `}>
+      {!isLocked &&
+        <DragIcon type="primary" />
+      }
+      <ConstructorElement
+        text={`${name}${type === "top" ? " верх" : type === "bottom" ? " низ" : ""}`}
+        thumbnail={image}
+        type={type}
+        price={price}
+        isLocked={isLocked}
+      />
+    </article>
   );
 }
