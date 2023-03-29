2f64c83909f762e3376c20a83c14818893b5ccca
Fedor Shendelov <shendelov@bigmir.net>
2023-03-30 04:07:01 +0800

    feat: add markup for ForgotPassword page

--- src/pages/forgot-password.jsx
+++ src/pages/forgot-password.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import VerticalForm from "../components/VerticalForm/vertical-form";
+
+import { FORGOT_PASS_DATA } from "../utils/constants";
 
 export default function ForgotPassword() {
+  const {heading, inputs, buttonCaption, links} =
+    FORGOT_PASS_DATA;
+
   return(
-    <h1>ForgotPassword stub</h1>
+    <VerticalForm
+      heading={heading}
+      inputs={inputs}
+      buttonCaption={buttonCaption}
+      links={links}
+    />
   )
 }
