b70d41527d64786b7b3646e5ec1676b714022ef6
Fedor Shendelov <shendelov@bigmir.net>
2023-03-29 23:29:09 +0800

    feat: add routing stubs for all pages, including 404

--- src/components/App/app.jsx
+++ src/components/App/app.jsx
@@ -4,6 +4,13 @@ import {Route, Routes} from "react-router-dom";
 import AppHeader from "../AppHeader/app-header";
 
 import Index from '../../pages';
+import FourOFour from '../../pages/four-o-four';
+import ForgotPassword from '../../pages/forgot-password';
+import Ingredients from '../../pages/ingredients-id';
+import Login from '../../pages/login';
+import Profile from '../../pages/profile';
+import Register from '../../pages/register';
+import ResetPassword from '../../pages/reset-password';
 
 import styles from "./app.module.css";
 
@@ -17,6 +24,13 @@ export default function App() {
         <main className={styles.main}>
           <Routes>
             <Route path='/' element={<Index />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/register' element={<Register />} />
+            <Route path='/forgot-password' element={<ForgotPassword />} />
+            <Route path='/reset-password' element={<ResetPassword />} />
+            <Route path='/profile' element={<Profile />} />
+            <Route path='/ingredients/:id' element={<Ingredients />} />
+            <Route path='*' element={<FourOFour />} />
           </Routes>
         </main>
       </>
