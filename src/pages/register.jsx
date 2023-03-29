3f2fbcb0ac30e00461fe90e0544d904bba478019
Fedor Shendelov <shendelov@bigmir.net>
2023-03-30 04:52:23 +0800

    feat: first attempt to connect to server
    Kinda successfull! Yay!

--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -7,12 +7,31 @@ export default function Register() {
   const {heading, inputs, buttonCaption, links} =
     REGISTER_DATA;
 
+  function registerUser(event) {
+    const body = {
+      email: event.target.email.value,
+      password: event.target.password.value,
+      name: event.target.name.value
+    }
+    fetch(
+      'https://norma.nomoreparties.space/api/auth/register',
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(body)
+      }
+    );
+  }
+
   return(
     <VerticalForm
       heading={heading}
       inputs={inputs}
       buttonCaption={buttonCaption}
       links={links}
+      callback={registerUser}
     />
   );
 }
