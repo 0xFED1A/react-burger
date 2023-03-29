e943c114c29cae3386dd0d4a1ebbd07c543666e4
Fedor Shendelov <shendelov@bigmir.net>
2023-03-30 03:58:13 +0800

    feat: add markup for Login page

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import VerticalForm from "../components/VerticalForm/vertical-form";
+
+import { LOGIN_DATA } from "../utils/constants";
 
 export default function Login() {
+  const {heading, inputs, buttonCaption, links} =
+    LOGIN_DATA;
+
   return(
-    <h1>Login stub</h1>
+    <VerticalForm
+      heading={heading}
+      inputs={inputs}
+      buttonCaption={buttonCaption}
+      links={links}
+    />
   )
 }
