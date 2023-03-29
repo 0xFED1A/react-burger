eb700817c8dc49185a6a629c1df221bfc3590472
Fedor Shendelov <shendelov@bigmir.net>
2023-03-30 04:12:57 +0800

    feat: add markup for ResetPassword page

--- src/pages/reset-password.jsx
+++ src/pages/reset-password.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import VerticalForm from "../components/VerticalForm/vertical-form";
+
+import { RESET_PASS_DATA } from "../utils/constants";
 
 export default function ResetPassword() {
+  const {heading, inputs, buttonCaption, links} =
+    RESET_PASS_DATA;
+
   return(
-    <h1>ResetPassword stub</h1>
+    <VerticalForm
+      heading={heading}
+      inputs={inputs}
+      buttonCaption={buttonCaption}
+      links={links}
+    />
   )
 }
