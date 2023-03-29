32bd716dea2c4c6acb49d5e8d435341a26f58de1
Fedor Shendelov <shendelov@bigmir.net>
2023-03-30 05:16:10 +0800

    feat: add some password reset functionality

--- src/pages/reset-password.jsx
+++ src/pages/reset-password.jsx
@@ -7,12 +7,29 @@ export default function ResetPassword() {
   const {heading, inputs, buttonCaption, links} =
     RESET_PASS_DATA;
 
+  function resetEmail(event) {
+    const body = {
+      password: event.target.password.value,
+      token: event.target.code.value
+    }
+    fetch(
+      'https://norma.nomoreparties.space/api/password-reset/reset',
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(body)
+      })
+  }
+
   return(
     <VerticalForm
       heading={heading}
       inputs={inputs}
       buttonCaption={buttonCaption}
       links={links}
+      callback={resetEmail}
     />
   )
 }
